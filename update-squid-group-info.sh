2bc38c84492e45c3a2df31fb785973bbf3d16a0d
Selivanov Pavel <selivan@users.noreply.github.com>
2014-03-20 19:32:50 +0300

    Update update-squid-group-info.sh

--- update-squid-group-info.sh
+++ update-squid-group-info.sh
@@ -4,6 +4,8 @@
 # Gets users for AD groups named as listed files
 # If group content changed, updates corresponding file
 
+cd /etc/squid3/groups/
+
 flag=`mktemp --tmpdir=/run`
 echo 0 > $flag
 
@@ -16,7 +18,6 @@ find /etc/squid3/groups/ -type f | while read file; do
     # Check, if group changed
     cmp "$file" "$temp" &> /dev/null
     if [ $? -ne 0 ]; then
-        echo '!!!!!!!!!!!!!!1'
         echo "Group $group in file $file updated"
         mv -f "$temp" "$file"
         echo 1 > $flag
@@ -26,6 +27,7 @@ find /etc/squid3/groups/ -type f | while read file; do
 done
 
 if [ `cat $flag` -eq 1 ]; then
+    date
     echo "Trying to reload squid3"
     service squid3 reload
     echo "Result: $?"
